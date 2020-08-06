ab42947323db7171638e1a057c8f2a60051414b7
Suran Basharati <suranbasharati@gmail.com>
2020-08-06 15:07:26 +0200

    Finished app

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      title: "I Am Poor",
       home: Scaffold(
-        backgroundColor: Colors.red[400],
         appBar: AppBar(
-          backgroundColor: Colors.red[700],
-          title: Text("I Am Poor"),
+          backgroundColor: Colors.orange[400],
+          title: Center(
+            child: Text("I Am Poor"),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/empty_box.png'),
+          ),
         ),
       ),
     ),
