6865f1eb169aa5eec85b54bd72bc36bc2a769d3f
Suran Basharati <suranbasharati@gmail.com>
2020-08-06 13:09:33 +0200

    FInished the first app, added project to second

---
+++ i_am_poor/android/app/src/main/kotlin/basharati/i_am_poor/MainActivity.kt
@@ -0,0 +1,6 @@
+package basharati.i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -19,17 +19,3 @@ void main() {
     ),
   );
 }
-
-/*
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-*/
