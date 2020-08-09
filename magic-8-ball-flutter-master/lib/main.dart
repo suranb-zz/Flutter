54f0bd9fa2dcda355aa5f7d81fbe9fdf3de513dc
Suran Basharati <suranbasharati@gmail.com>
2020-08-09 10:14:32 +0200

    Finished the recap project

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -1,9 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
 
 
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade400,
+      appBar: AppBar(
+        title: Center(
+          child: Text('Ask me anything'),
+        ),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  int changePicture(){
+    return ballNumber = Random().nextInt(5) + 1;
+    print('Function |$changePicture()| was called');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+            onPressed: () {
+              setState(() {
+                changePicture();
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png')),
+      ),
+    );
+  }
+}
