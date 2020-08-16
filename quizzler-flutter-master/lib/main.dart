972850f3ff189ecacf77d0394f327af4a295e154
Suran Basharati <suranbasharati@gmail.com>
2020-08-16 14:19:08 +0200

    Add Exercism

--- quizzler-flutter-master/lib/main.dart
+++ quizzler-flutter-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,17 +29,12 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper = [];
 
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+  Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+  Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
 
   int questionNumber = 0;
 
@@ -54,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -79,7 +75,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
 
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
 
                 if (correctAnswer){
                   print('User got it right');
@@ -111,7 +107,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
 
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
 
                 if (!correctAnswer){
                   print('User got it right');
